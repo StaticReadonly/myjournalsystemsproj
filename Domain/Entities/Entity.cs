7b2dc4dd228b3858203aae3057465ec4779f63f0
StaticReadonly <klymukvlad4002@gmail.com>
2026-04-10 22:57:08 +0300

    base entity

---
+++ Domain/Entities/Entity.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain.Entities
+{
+    public abstract class Entity<T>
+    {
+        public T Id { get; private set; }
+
+        protected Entity(T id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Entity<T> other)
+            {
+                return false;
+            }
+            else if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            else if (EqualityComparer<T>.Default.Equals(Id, default) || EqualityComparer<T>.Default.Equals(other.Id, default))
+            {
+                return false;
+            }
+
+            return EqualityComparer<T>.Default.Equals(Id, other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id?.GetHashCode() ?? 0;
+        }
+    }
+}
